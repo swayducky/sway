af7488d0df7b83deb17501ae05c00416f834d84f
Sway Ducky <swayducky@gmail.com>
2023-06-04 11:40:43 -0700

    anthropic

--- anth/main.py
+++ anth/main.py
@@ -1,17 +1,28 @@
-import os
+"""
+See: https://github.com/anthropics/anthropic-sdk-python/tree/main/examples
+"""
 import anthropic
+import os
+from dotenv import load_dotenv
 
-
-def main(max_tokens_to_sample: int = 100):
-    c = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
-    resp = c.completion(
-        prompt=f"{anthropic.HUMAN_PROMPT} How many toes do dogs have?{anthropic.AI_PROMPT}",
+def main(max_tokens_to_sample: int = 200):
+    anth = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
+    prompt = open('prompt.txt').read()
+    print(prompt)
+    confirm = input('>> Enter "y" to continue, with the above prompt: ')
+    if confirm != 'y':
+        return
+    response = anth.completion_stream(
+        prompt=f"{anthropic.HUMAN_PROMPT} {prompt}{anthropic.AI_PROMPT}",
         stop_sequences=[anthropic.HUMAN_PROMPT],
-        model="claude-v1",
         max_tokens_to_sample=max_tokens_to_sample,
+        model="claude-v1",
+        stream=True,
     )
-    print(resp)
+    for data in response:
+        print(data['completion'])
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
