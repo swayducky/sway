e8c2ecbee51bc832aad6af39ebd819962c4ac5ca
Sway Ducky <swayducky@gmail.com>
2023-06-13 00:30:12 -0700

    initial stab

---
+++ circle/main.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageOps, ImageDraw
+import os
+
+def circle_crop(path):
+    image = Image.open(path)
+    image = image.convert("RGBA") # Ensure alpha layer
+
+    # Crop in the middle, width-wise and height-wise.
+    width, height = image.size
+    if width != height:
+        min_side = min(width, height)
+        left = (width - min_side) / 2
+        top = (height - min_side) / 2
+        right = (width + min_side) / 2
+        bottom = (height + min_side) / 2
+        image = image.crop((left, top, right, bottom))
+
+    # Make the image circular
+    mask = Image.new("L", image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, image.size[0], image.size[1]), fill=255)
+
+    result = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
+    result.putalpha(mask)
+
+    # Add a border
+    border_color = "#0AAD0A"
+    border = Image.new("RGB", (result.width + 2, result.height + 2), border_color)
+    border.paste(result, (1, 1))
+    return border
+
+def process_directory(directory):
+    for filename in os.listdir(directory):
+        print(filename)
+        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            path = os.path.join(directory, filename)
+            new_image = circle_crop(path)
+            new_image.save(os.path.join("_out", "circle_" + filename)) # Save new images with a prefix
+
+process_directory("_images")
