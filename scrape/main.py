aed4e3294404c80e8904620712f8c2977e5ab7b1
Sway Ducky <swayducky@gmail.com>
2023-07-06 01:08:21 -0700

    scraper

--- scrape/main.py
+++ scrape/main.py
@@ -1,12 +1,26 @@
 import requests
 from html2text import html2text
+import tiktoken
+
 
 def scrape(url):
     response = requests.get(url)
     return html2text(response.text)
 
-if __name__ == "__main__":
-    text = scrape('https://www.getcerta.com/')
-    print(text)
+def dump():
+    text = scrape('https://www.getcerta.com/contract')
     with open('page.txt', 'w') as f:
         f.write(text)
+    print("Saved!", len(text), "chars")
+
+
+def measure():
+    with open('page.txt', 'r') as f:
+        text = f.read()
+    enc = tiktoken.encoding_for_model("gpt-4")
+    encoding = enc.encode(text)
+    print(len(encoding), 'tokens!')
+
+if __name__ == "__main__":
+    dump()
+    measure()
