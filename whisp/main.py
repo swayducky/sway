2bbdb681440abaa5786717a1aa8d8d27421bb9d6
Sway Ducky <swayducky@gmail.com>
2023-08-15 18:32:41 -0700

    poetry init

---
+++ whisp/main.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+Source = 'Youtube'
+video_url = "https://youtu.be/hpZFJctBUHQ"
+video_path = "/content/drive/MyDrive/Customer_Service.mp3"
+output_path = "/content/transcript/"
+output_path = str(Path(output_path))
+audio_title = "Sample Order Taking"
+access_token = "hf_"
+
