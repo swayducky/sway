c8631b6f47165ce6d3c6764388f7e1ae99fc22cf
Sway Ducky <swayducky@gmail.com>
2023-08-16 20:47:10 -0700

    working ocr

--- monitor/main.py
+++ monitor/main.py
@@ -1,15 +1,40 @@
+from PIL import ImageGrab
+import pytesseract
+import time
 import Quartz
+from AppKit import NSRunningApplication, NSWorkspace
 
-active_app = Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionOnScreenOnly | Quartz.kCGWindowListExcludeDesktopElements, Quartz.kCGNullWindowID)[0]['kCGWindowOwnerName']
+def get_active_window_title():
+    options = Quartz.kCGWindowListOptionOnScreenOnly | Quartz.kCGWindowListExcludeDesktopElements
+    window_list = Quartz.CGWindowListCopyWindowInfo(options, Quartz.kCGNullWindowID)
+    active_app_pid = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationProcessIdentifier']
+    active_app_windows = [window for window in window_list if window['kCGWindowOwnerPID'] == active_app_pid]
 
-print(active_app)
+    for window in active_app_windows:
+        if 'kCGWindowName' in window:
+            return window['kCGWindowName']
 
-import AppKit
+    return None
 
-active_app = Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionOnScreenOnly | Quartz.kCGWindowListExcludeDesktopElements, Quartz.kCGNullWindowID)[0]['kCGWindowOwnerName']
+def screenshot_ocr():
 
-app = AppKit.NSRunningApplication.runningApplicationWithProcessIdentifier_(int(active_app.split(' ')[0]))
+    # Take a screenshot
+    screenshot = ImageGrab.grab()
 
-text = app.activateWithOptions_(AppKit.NSApplicationActivateIgnoringOtherApps).windows[0].delegate.windowShouldClose_(None)
+    # Run OCR on the image
+    text = pytesseract.image_to_string(screenshot)
+    # screenshot.save('screenshot.png')
 
-print(text)
+    # Print the detected text
+    print(text)
+
+while True:
+    active_app = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
+    active_window_title = get_active_window_title()
+    print("Active app:", active_app)
+    if active_window_title:
+        print("Active window title:", active_window_title)
+    else:
+        print("No active window title found")
+    screenshot_ocr()
+    time.sleep(1.0)
